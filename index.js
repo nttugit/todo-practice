be3009c614dcabd21e92e2be51cdc00e76d7edcf
lathanhtuan <lathanhtuan1py@gmail.com>
2023-11-21 18:24:22 +0700

    Add tuan.js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 // Kết nối database
 import connectDB from './utils/db.js';
 

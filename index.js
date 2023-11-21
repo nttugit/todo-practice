4e9342e8fb11996e05707bc793041e5c044022d4
nttu <nttugpt@gmail.com>
2023-11-21 17:52:19 +0700

    Update connecting to DB

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from 'express';
+// Thư viện giúp sử dụng biến môi trường trong file .env
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+// Kết nối database
 import connectDB from './utils/db.js';
-connectDB();
 
+// Routes
 import userRouter from './routes/user.route.js';
 import todoRouter from './routes/todo.route.js';
 
+// Giúp gửi dữ liệu với method POST
 app.use(express.json());
 app.use('/users', userRouter);
 app.use('/todos', todoRouter);
--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,35 @@
 import mongoose from 'mongoose';
-const dbURI = 'mongodb://localhost:27017/ads-management';
 
-const connectDB = () => {
-    mongoose
-        .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log('Connected to MongoDB: ', dbURI);
-        })
-        .catch((err) => {
-            console.error('Error connecting to MongoDB: ', err);
-        });
-};
+// Connection URL
+const dbUrl = 'mongodb://127.0.0.1:27017/todo-practice';
 
-export default connectDB;
+// Connect to MongoDB
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Event listener for successful connection
+db.on('connected', () => {
+    console.log(`Connected to MongoDB at ${dbUrl}`);
+});
+
+// Event listener for connection errors
+db.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+});
+
+// Event listener for disconnection
+db.on('disconnected', () => {
+    console.log('Disconnected from MongoDB');
+});
+
+// Close the Mongoose connection when the Node process is terminated
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose disconnected through app termination');
+        process.exit(0);
+    });
+});
+
+export default db;
